05031524dd1ce0c290cab3df8d366d3b08bf6575
andreash <logins@andreash.no>
2026-02-05 10:46:19 +0100

    Manhattan to implement

--- src/heuristics.rs
+++ src/heuristics.rs
@@ -22,10 +22,30 @@ impl Heuristic {
             // blind heuristic always returns 0
             Heuristic::Blind => 0,
             Heuristic::Hamming => {
-                todo!()
+                let mut sum = 0;
+                for i in 0..=8 {
+                    if !(board.value_at(i/3, i%3) == i as u8) {
+                        sum +=1;
+                    }
+                }
+                sum
             }
+            // TODO
+            // Can't just use sort, since 0 is at the end of the list and not at the beginning
             Heuristic::Manhattan => {
-                todo!()
+                let mut sum = 0;
+                for i in 1..=8 {
+                    let pos = board.position(i);
+
+                    let index: i8 = i as i8;
+                    let row: i8 = pos.0 as i8;
+                    let col: i8 = pos.1 as i8;
+
+
+                    sum += (row - (index/3)).abs() + (col - index%3).abs();
+                    println!("{} {}", row, index/3);
+                }
+                sum as u32
             }
         }
     }
@@ -39,7 +59,7 @@ mod tests {
         use super::*;
         let board = Board::new([[8, 7, 3], [2, 0, 5], [1, 4, 6]]);
         assert_eq!(Heuristic::Blind.estimate(&board), 0);
-        assert_eq!(Heuristic::Hamming.estimate(&board), todo!());
-        assert_eq!(Heuristic::Manhattan.estimate(&board), todo!());
+        assert_eq!(Heuristic::Hamming.estimate(&board), 2+3+3);
+        assert_eq!(Heuristic::Manhattan.estimate(&board), 16);
     }
 }
